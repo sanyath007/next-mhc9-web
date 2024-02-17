f33893ca9cc9ebbd095a7be8dbd2a8f5e206bada
sanyath007 <sanyath007@gmail.com>
2024-02-17 11:32:33 +0700

    reflactor(Home component): design all its structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,32 +4,62 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="wrapper w-full">
+            {/* Heros */}
+            <div className="hero bg-white relative">
+                {/* Slide Heros */}
+                <section className="border-2 border-red-500 h-full">
+                    <div className="container">
+                        Hero
+                    </div>
+                </section>
+
+                {/* Option Menus */}
+                <section className="h-[60px] absolute -bottom-2 w-full bg-gray-100">
+
+                </section>
+            </div>
+
             <div className="container">
-                <h1 className="text-2xl font-bold">Home Page</h1>
-
-                <hr className="mt-2" />
-
                 <div className="row mt-4">
                     <div className="col-md-12 min-h-screen flex flex-col justify-between">
-                        {/* Hero */}
+                        {/* News Headlines */}
+                        <section className="border-2 border-red-500">
+                            {/* News Headlines */}
+
+                            {/* Director */}
+                        </section>
+
+                        {/* Videos */}
+                        <section className="border-2 border-red-500">
+                            {/* Videos */}
+
+                            {/* Officer Services */}
+                        </section>
+
+                        {/* News */}
                         <section className="border-2 border-red-500">
 
                         </section>
-                        {/* Option Menus */}
-                        <section className="border-2 border-red-500"></section>
-                        {/* News Headlines */}
-                        <section className="border-2 border-red-500"></section>
-                        {/* Videos */}
-                        <section className="border-2 border-red-500"></section>
-                        {/* News */}
-                        <section className="border-2 border-red-500"></section>
+
+                        {/* One pages */}
+                        <section className="border-2 border-red-500">
+
+                        </section>
+
                         {/* Articles */}
-                        <section className="border-2 border-red-500"></section>
+                        <section className="border-2 border-red-500">
+
+                        </section>
+
+                        {/* E-Services */}
+                        <section className="border-2 border-red-500">
+
+                        </section>
                     </div>
                 </div>
             </div>
 
-            {/* Slide Logos */}
+            {/* Slide Bottom Logos */}
             <div className="h-[150px] bg-white p-4">
                 <SlideLogos />
             </div>
