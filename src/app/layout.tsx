96ca9e02f2e180506939cf50cf5f2b20c208c897
sanyath007 <sanyath007@gmail.com>
2024-02-19 10:19:12 +0700

    style(layout.tsx): remove space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Kanit  } from "next/font/google";
+import { Kanit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
